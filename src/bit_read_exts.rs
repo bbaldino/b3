4094fc272408d347e085f6046e68949de37586fd
brian baldino <bbaldino@gmail.com>
2022-11-30 23:27:15 -0800

    get rid of some use of macros to impl trait methods, i wasn't getting auto completion for them(?!)

--- src/bit_read_exts.rs
+++ src/bit_read_exts.rs
@@ -1,53 +1,201 @@
-use paste::paste;
+use std::ops::{ShlAssign, BitOrAssign};
+
 use ux::*;
 
 use crate::{bit_read::BitRead, byte_order::ByteOrder};
 
-macro_rules! impl_bit_read {
-    ($type:ty, $size_bits:expr) => {
-        paste! {
-            fn [<read_ $type>](&mut self) -> $type {
-                let mut buf = [u1::new(0); $size_bits];
-                self.read_exact(&mut buf).unwrap();
-                let mut val = <$type>::new(0);
-                for i in 0..$size_bits {
-                    val <<= 1;
-                    val |= buf[i].into();
-                }
-                val
-            }
-        }
-    };
+fn bit_read_exts_helper<T, const N: usize, U: BitRead>(buf: &mut U) -> T
+where
+    T: Default + ShlAssign<usize> + BitOrAssign + From<u1>,
+    U: ?Sized
+{
+
+    let mut read_buf = [u1::default(); N];
+    buf.read_exact(&mut read_buf).unwrap();
+    let mut val = T::default();
+    for bit in read_buf.iter() {
+        val <<= 1;
+        val |= (*bit).into();
+    }
+    val
 }
 
-// /// Macro for generating BitReadExts read methods for types larger than 8 bits (which require a
-// /// ByteOrder parameter).
-// /// TODO: this doesn't work, perhaps because paste! doesn't like the nested brackets.
-// /// See https://github.com/dtolnay/paste/issues/90
-// macro_rules! impl_bit_read_bo {
-//     ($type:ty, $size_bits:expr) => {
-//         paste! {
-//             fn [<read_ $type>]<T: ByteOrder>(&mut self) -> $type {
-//                 let mut buf = [u1::new(0); $size_bits];
-//                 self.read_exact(&mut buf).unwrap();
-//                 <T>::read_u9(&buf)
-//                 //[< <T>::read_ $type>](&buf)
-//             }
-//         }
-//     };
-// }
 
 pub trait BitReadExts: BitRead {
-    impl_bit_read!(u2, 2);
-    impl_bit_read!(u3, 3);
-    impl_bit_read!(u4, 4);
-    impl_bit_read!(u5, 5);
+    fn read_u1(&mut self) -> u1 {
+        bit_read_exts_helper::<u1, 1, Self>(self)
+    }
 
-    // See note on impl_bit_read_bo as to why we can't use the macro
+    fn read_u2(&mut self) -> u2 {
+        bit_read_exts_helper::<u2, 2, Self>(self)
+    }
+
+    fn read_u3(&mut self) -> u3 {
+        bit_read_exts_helper::<u3, 3, Self>(self)
+    }
+
+    fn read_u4(&mut self) -> u4 {
+        bit_read_exts_helper::<u4, 4, Self>(self)
+    }
+
+    fn read_u5(&mut self) -> u5 {
+        bit_read_exts_helper::<u5, 5, Self>(self)
+    }
+
+    fn read_u6(&mut self) -> u6 {
+        bit_read_exts_helper::<u6, 6, Self>(self)
+    }
+
+    fn read_u7(&mut self) -> u7 {
+        bit_read_exts_helper::<u7, 7, Self>(self)
+    }
+
+    fn read_u8(&mut self) -> u8 {
+        bit_read_exts_helper::<u8, 8, Self>(self)
+    }
+
     fn read_u9<T: ByteOrder>(&mut self) -> u9 {
         let mut buf = [u1::new(0); 9];
         self.read_exact(&mut buf).unwrap();
         <T>::read_u9(&buf)
+    }
+
+    fn read_u10<T: ByteOrder>(&mut self) -> u10 {
+        let mut buf = [u1::new(0); 10];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u10(&buf)
+    }
+
+    fn read_u11<T: ByteOrder>(&mut self) -> u11 {
+        let mut buf = [u1::new(0); 11];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u11(&buf)
+    }
+
+    fn read_u12<T: ByteOrder>(&mut self) -> u12 {
+        let mut buf = [u1::new(0); 12];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u12(&buf)
+    }
+
+    fn read_u13<T: ByteOrder>(&mut self) -> u13 {
+        let mut buf = [u1::new(0); 13];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u13(&buf)
+    }
+
+    fn read_u14<T: ByteOrder>(&mut self) -> u14 {
+        let mut buf = [u1::new(0); 14];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u14(&buf)
+    }
+
+    fn read_u15<T: ByteOrder>(&mut self) -> u15 {
+        let mut buf = [u1::new(0); 15];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u15(&buf)
+    }
+
+    fn read_u16<T: ByteOrder>(&mut self) -> u16 {
+        let mut buf = [u1::new(0); 16];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u16(&buf)
+    }
+
+    fn read_u17<T: ByteOrder>(&mut self) -> u17 {
+        let mut buf = [u1::new(0); 17];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u17(&buf)
+    }
+
+    fn read_u18<T: ByteOrder>(&mut self) -> u18 {
+        let mut buf = [u1::new(0); 18];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u18(&buf)
+    }
+
+    fn read_u19<T: ByteOrder>(&mut self) -> u19 {
+        let mut buf = [u1::new(0); 19];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u19(&buf)
+    }
+
+    fn read_u20<T: ByteOrder>(&mut self) -> u20 {
+        let mut buf = [u1::new(0); 20];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u20(&buf)
+    }
+
+    fn read_u21<T: ByteOrder>(&mut self) -> u21 {
+        let mut buf = [u1::new(0); 21];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u21(&buf)
+    }
+
+    fn read_u22<T: ByteOrder>(&mut self) -> u22 {
+        let mut buf = [u1::new(0); 22];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u22(&buf)
+    }
+
+    fn read_u23<T: ByteOrder>(&mut self) -> u23 {
+        let mut buf = [u1::new(0); 23];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u23(&buf)
+    }
+
+    fn read_u24<T: ByteOrder>(&mut self) -> u24 {
+        let mut buf = [u1::new(0); 24];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u24(&buf)
+    }
+
+    fn read_u25<T: ByteOrder>(&mut self) -> u25 {
+        let mut buf = [u1::new(0); 25];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u25(&buf)
+    }
+
+    fn read_u26<T: ByteOrder>(&mut self) -> u26 {
+        let mut buf = [u1::new(0); 26];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u26(&buf)
+    }
+
+    fn read_u27<T: ByteOrder>(&mut self) -> u27 {
+        let mut buf = [u1::new(0); 27];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u27(&buf)
+    }
+
+    fn read_u28<T: ByteOrder>(&mut self) -> u28 {
+        let mut buf = [u1::new(0); 28];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u28(&buf)
+    }
+
+    fn read_u29<T: ByteOrder>(&mut self) -> u29 {
+        let mut buf = [u1::new(0); 29];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u29(&buf)
+    }
+
+    fn read_u30<T: ByteOrder>(&mut self) -> u30 {
+        let mut buf = [u1::new(0); 30];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u30(&buf)
+    }
+
+    fn read_u31<T: ByteOrder>(&mut self) -> u31 {
+        let mut buf = [u1::new(0); 31];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u31(&buf)
+    }
+
+    fn read_u32<T: ByteOrder>(&mut self) -> u32 {
+        let mut buf = [u1::new(0); 32];
+        self.read_exact(&mut buf).unwrap();
+        <T>::read_u32(&buf)
     }
 }
 
--- src/byte_order.rs
+++ src/byte_order.rs
@@ -8,31 +8,31 @@ macro_rules! impl_read_le {
     ($type:ty, $size_bits:expr) => {
         paste! {
             fn [<read_ $type>](buf: &[u1; $size_bits]) -> $type {
-                let mut val = <$type>::new(0);
+                let mut val = <$type>::default();
                 if $size_bits > 32 {
                     unimplemented!("Only uX types up to u32 supported");
                 }
                 if $size_bits > 24 {
                     for i in 24..std::cmp::min($size_bits, 32) {
                         val <<= 1;
-                        val |= buf[i].into();
+                        val |= <ux::u1 as Into<$type>>::into(buf[i]);
                     }
                 }
                 if $size_bits > 16 {
                     for i in 16..std::cmp::min($size_bits, 24) {
                         val <<= 1;
-                        val |= buf[i].into();
+                        val |= <ux::u1 as Into<$type>>::into(buf[i]);
                     }
                 }
                 if $size_bits > 8 {
                     for i in 8..std::cmp::min($size_bits, 16) {
                         val <<= 1;
-                        val |= buf[i].into();
+                        val |= <ux::u1 as Into<$type>>::into(buf[i]);
                     }
                 }
                 for i in 0..std::cmp::min($size_bits, 8) {
                     val <<= 1;
-                    val |= buf[i].into();
+                    val |= <ux::u1 as Into<$type>>::into(buf[i]);
                 }
                 val
             }
@@ -41,10 +41,38 @@ macro_rules! impl_read_le {
 }
 
 pub trait ByteOrder {
+    fn read_u1(buf: &[u1; 1]) -> u1;
+    fn read_u2(buf: &[u1; 2]) -> u2;
+    fn read_u3(buf: &[u1; 3]) -> u3;
+    fn read_u4(buf: &[u1; 4]) -> u4;
+    fn read_u5(buf: &[u1; 5]) -> u5;
+    fn read_u6(buf: &[u1; 6]) -> u6;
+    fn read_u7(buf: &[u1; 7]) -> u7;
+    fn read_u8(buf: &[u1; 8]) -> u8;
     fn read_u9(buf: &[u1; 9]) -> u9;
+    fn read_u10(buf: &[u1; 10]) -> u10;
     fn read_u11(buf: &[u1; 11]) -> u11;
     fn read_u12(buf: &[u1; 12]) -> u12;
+    fn read_u13(buf: &[u1; 13]) -> u13;
+    fn read_u14(buf: &[u1; 14]) -> u14;
+    fn read_u15(buf: &[u1; 15]) -> u15;
+    fn read_u16(buf: &[u1; 16]) -> u16;
+    fn read_u17(buf: &[u1; 17]) -> u17;
     fn read_u18(buf: &[u1; 18]) -> u18;
+    fn read_u19(buf: &[u1; 19]) -> u19;
+    fn read_u20(buf: &[u1; 20]) -> u20;
+    fn read_u21(buf: &[u1; 21]) -> u21;
+    fn read_u22(buf: &[u1; 22]) -> u22;
+    fn read_u23(buf: &[u1; 23]) -> u23;
+    fn read_u24(buf: &[u1; 24]) -> u24;
+    fn read_u25(buf: &[u1; 25]) -> u25;
+    fn read_u26(buf: &[u1; 26]) -> u26;
+    fn read_u27(buf: &[u1; 27]) -> u27;
+    fn read_u28(buf: &[u1; 28]) -> u28;
+    fn read_u29(buf: &[u1; 29]) -> u29;
+    fn read_u30(buf: &[u1; 30]) -> u30;
+    fn read_u31(buf: &[u1; 31]) -> u31;
+    fn read_u32(buf: &[u1; 32]) -> u32;
 }
 
 pub struct BigEndian {}
@@ -58,10 +86,38 @@ pub type NetworkOrder = BigEndian;
 // method?
 
 impl ByteOrder for LittleEndian {
+    impl_read_le!(u1, 1);
+    impl_read_le!(u2, 2);
+    impl_read_le!(u3, 3);
+    impl_read_le!(u4, 4);
+    impl_read_le!(u5, 5);
+    impl_read_le!(u6, 6);
+    impl_read_le!(u7, 7);
+    impl_read_le!(u8, 8);
     impl_read_le!(u9, 9);
+    impl_read_le!(u10, 10);
     impl_read_le!(u11, 11);
     impl_read_le!(u12, 12);
+    impl_read_le!(u13, 13);
+    impl_read_le!(u14, 14);
+    impl_read_le!(u15, 15);
+    impl_read_le!(u16, 16);
+    impl_read_le!(u17, 17);
     impl_read_le!(u18, 18);
+    impl_read_le!(u19, 19);
+    impl_read_le!(u20, 20);
+    impl_read_le!(u21, 21);
+    impl_read_le!(u22, 22);
+    impl_read_le!(u23, 23);
+    impl_read_le!(u24, 24);
+    impl_read_le!(u25, 25);
+    impl_read_le!(u26, 26);
+    impl_read_le!(u27, 27);
+    impl_read_le!(u28, 28);
+    impl_read_le!(u29, 29);
+    impl_read_le!(u30, 30);
+    impl_read_le!(u31, 31);
+    impl_read_le!(u32, 32);
 }
 
 fn big_endian_read_helper<T, const N: usize>(buf: &[u1; N]) -> T
@@ -77,7 +133,43 @@ where
 }
 
 impl ByteOrder for BigEndian {
+    fn read_u1(buf: &[u1; 1]) -> u1 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u2(buf: &[u1; 2]) -> u2 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u3(buf: &[u1; 3]) -> u3 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u4(buf: &[u1; 4]) -> u4 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u5(buf: &[u1; 5]) -> u5 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u6(buf: &[u1; 6]) -> u6 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u7(buf: &[u1; 7]) -> u7 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u8(buf: &[u1; 8]) -> u8 {
+        big_endian_read_helper(buf)
+    }
+
     fn read_u9(buf: &[u1; 9]) -> u9 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u10(buf: &[u1; 10]) -> u10 {
         big_endian_read_helper(buf)
     }
 
@@ -89,7 +181,83 @@ impl ByteOrder for BigEndian {
         big_endian_read_helper(buf)
     }
 
+    fn read_u13(buf: &[u1; 13]) -> u13 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u14(buf: &[u1; 14]) -> u14 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u15(buf: &[u1; 15]) -> u15 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u16(buf: &[u1; 16]) -> u16 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u17(buf: &[u1; 17]) -> u17 {
+        big_endian_read_helper(buf)
+    }
+
     fn read_u18(buf: &[u1; 18]) -> u18 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u19(buf: &[u1; 19]) -> u19 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u20(buf: &[u1; 20]) -> u20 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u21(buf: &[u1; 21]) -> u21 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u22(buf: &[u1; 22]) -> u22 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u23(buf: &[u1; 23]) -> u23 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u24(buf: &[u1; 24]) -> u24 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u25(buf: &[u1; 25]) -> u25 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u26(buf: &[u1; 26]) -> u26 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u27(buf: &[u1; 27]) -> u27 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u28(buf: &[u1; 28]) -> u28 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u29(buf: &[u1; 29]) -> u29 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u30(buf: &[u1; 30]) -> u30 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u31(buf: &[u1; 31]) -> u31 {
+        big_endian_read_helper(buf)
+    }
+
+    fn read_u32(buf: &[u1; 32]) -> u32 {
         big_endian_read_helper(buf)
     }
 }
