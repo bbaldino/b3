ae612194d6f39f695bbd29ff70deb7292efb47f3
brian baldino <bbaldino@gmail.com>
2023-02-11 17:55:21 -0800

    add byteorder for relevant write operations

--- src/bit_write_exts.rs
+++ src/bit_write_exts.rs
@@ -1,6 +1,6 @@
 use ux::*;
 
-use crate::{bit_traits::BitTraits, bit_write::BitWrite};
+use crate::{bit_traits::BitTraits, bit_write::BitWrite, byte_order::ByteOrder};
 
 fn bit_write_exts_helper<T: BitTraits, const N: usize, U: BitWrite + ?Sized>(
     buf: &mut U,
@@ -58,100 +58,148 @@ pub trait BitWriteExts: BitWrite {
         bit_write_exts_helper::<u8, 8, Self>(self, value)
     }
 
-    fn write_u9(&mut self, value: u9) -> std::io::Result<()> {
-        bit_write_exts_helper::<u9, 9, Self>(self, value)
+    fn write_u9<T: ByteOrder>(&mut self, value: u9) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 9];
+        T::write_u9(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u10(&mut self, value: u10) -> std::io::Result<()> {
-        bit_write_exts_helper::<u10, 10, Self>(self, value)
+    fn write_u10<T: ByteOrder>(&mut self, value: u10) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 10];
+        T::write_u10(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u11(&mut self, value: u11) -> std::io::Result<()> {
-        bit_write_exts_helper::<u11, 11, Self>(self, value)
+    fn write_u11<T: ByteOrder>(&mut self, value: u11) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 11];
+        T::write_u11(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u12(&mut self, value: u12) -> std::io::Result<()> {
-        bit_write_exts_helper::<u12, 12, Self>(self, value)
+    fn write_u12<T: ByteOrder>(&mut self, value: u12) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 12];
+        T::write_u12(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u13(&mut self, value: u13) -> std::io::Result<()> {
-        bit_write_exts_helper::<u13, 13, Self>(self, value)
+    fn write_u13<T: ByteOrder>(&mut self, value: u13) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 13];
+        T::write_u13(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u14(&mut self, value: u14) -> std::io::Result<()> {
-        bit_write_exts_helper::<u14, 14, Self>(self, value)
+    fn write_u14<T: ByteOrder>(&mut self, value: u14) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 14];
+        T::write_u14(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u15(&mut self, value: u15) -> std::io::Result<()> {
-        bit_write_exts_helper::<u15, 15, Self>(self, value)
+    fn write_u15<T: ByteOrder>(&mut self, value: u15) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 15];
+        T::write_u15(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u16(&mut self, value: u16) -> std::io::Result<()> {
-        bit_write_exts_helper::<u16, 16, Self>(self, value)
+    fn write_u16<T: ByteOrder>(&mut self, value: u16) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 16];
+        T::write_u16(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u17(&mut self, value: u17) -> std::io::Result<()> {
-        bit_write_exts_helper::<u17, 17, Self>(self, value)
+    fn write_u17<T: ByteOrder>(&mut self, value: u17) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 17];
+        T::write_u17(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u18(&mut self, value: u18) -> std::io::Result<()> {
-        bit_write_exts_helper::<u18, 18, Self>(self, value)
+    fn write_u18<T: ByteOrder>(&mut self, value: u18) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 18];
+        T::write_u18(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u19(&mut self, value: u19) -> std::io::Result<()> {
-        bit_write_exts_helper::<u19, 19, Self>(self, value)
+    fn write_u19<T: ByteOrder>(&mut self, value: u19) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 19];
+        T::write_u19(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u20(&mut self, value: u20) -> std::io::Result<()> {
-        bit_write_exts_helper::<u20, 20, Self>(self, value)
+    fn write_u20<T: ByteOrder>(&mut self, value: u20) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 20];
+        T::write_u20(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u21(&mut self, value: u21) -> std::io::Result<()> {
-        bit_write_exts_helper::<u21, 21, Self>(self, value)
+    fn write_u21<T: ByteOrder>(&mut self, value: u21) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 21];
+        T::write_u21(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u22(&mut self, value: u22) -> std::io::Result<()> {
-        bit_write_exts_helper::<u22, 22, Self>(self, value)
+    fn write_u22<T: ByteOrder>(&mut self, value: u22) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 22];
+        T::write_u22(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u23(&mut self, value: u23) -> std::io::Result<()> {
-        bit_write_exts_helper::<u23, 23, Self>(self, value)
+    fn write_u23<T: ByteOrder>(&mut self, value: u23) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 23];
+        T::write_u23(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u24(&mut self, value: u24) -> std::io::Result<()> {
-        bit_write_exts_helper::<u24, 24, Self>(self, value)
+    fn write_u24<T: ByteOrder>(&mut self, value: u24) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 24];
+        T::write_u24(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u25(&mut self, value: u25) -> std::io::Result<()> {
-        bit_write_exts_helper::<u25, 25, Self>(self, value)
+    fn write_u25<T: ByteOrder>(&mut self, value: u25) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 25];
+        T::write_u25(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u26(&mut self, value: u26) -> std::io::Result<()> {
-        bit_write_exts_helper::<u26, 26, Self>(self, value)
+    fn write_u26<T: ByteOrder>(&mut self, value: u26) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 26];
+        T::write_u26(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u27(&mut self, value: u27) -> std::io::Result<()> {
-        bit_write_exts_helper::<u27, 27, Self>(self, value)
+    fn write_u27<T: ByteOrder>(&mut self, value: u27) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 27];
+        T::write_u27(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u28(&mut self, value: u28) -> std::io::Result<()> {
-        bit_write_exts_helper::<u28, 28, Self>(self, value)
+    fn write_u28<T: ByteOrder>(&mut self, value: u28) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 28];
+        T::write_u28(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u29(&mut self, value: u29) -> std::io::Result<()> {
-        bit_write_exts_helper::<u29, 29, Self>(self, value)
+    fn write_u29<T: ByteOrder>(&mut self, value: u29) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 29];
+        T::write_u29(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u30(&mut self, value: u30) -> std::io::Result<()> {
-        bit_write_exts_helper::<u30, 30, Self>(self, value)
+    fn write_u30<T: ByteOrder>(&mut self, value: u30) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 30];
+        T::write_u30(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u31(&mut self, value: u31) -> std::io::Result<()> {
-        bit_write_exts_helper::<u31, 31, Self>(self, value)
+    fn write_u31<T: ByteOrder>(&mut self, value: u31) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 31];
+        T::write_u31(&mut arr, value);
+        self.write_all(&arr)
     }
 
-    fn write_u32(&mut self, value: u32) -> std::io::Result<()> {
-        bit_write_exts_helper::<u32, 32, Self>(self, value)
+    fn write_u32<T: ByteOrder>(&mut self, value: u32) -> std::io::Result<()> {
+        let mut arr = [u1::default(); 32];
+        T::write_u32(&mut arr, value);
+        self.write_all(&arr)
     }
 }
 
@@ -159,7 +207,7 @@ impl<T> BitWriteExts for T where T: BitWrite {}
 
 #[cfg(test)]
 mod tests {
-    use crate::{bit_cursor::BitCursor, bitarray, bitvec};
+    use crate::{bit_cursor::BitCursor, bitarray, bitvec, byte_order::NetworkOrder};
 
     use super::*;
 
@@ -173,7 +221,9 @@ mod tests {
 
         let vec = bitvec!(0; 12);
         let mut cursor = BitCursor::new(vec);
-        assert!(cursor.write_u12(u12::new(0b110011001100)).is_ok());
+        assert!(cursor
+            .write_u12::<NetworkOrder>(u12::new(0b110011001100))
+            .is_ok());
         assert_eq!(
             cursor.into_inner().get_slice(..),
             &bitarray!(1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0)[..]
